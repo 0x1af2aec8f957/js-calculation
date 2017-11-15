2c0a6b76a59930eaa97f5ec50d9b5f0cd2b245ec
川 <603803799@qq.com>
2017-11-14 20:54:52 -0600

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
  */
 (function (global, factory) {
     typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
-        typeof define === 'function' && define.amd ? define(factory) : (global.accuracy = factory());
+        typeof define === 'function' && define.amd ? define(factory) : (global.calculation = factory());
 })(this, function () {
     return {
         division(arg1, arg2) { //除法
