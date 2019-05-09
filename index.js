42fc49977fce9f2e855e6ef3a50cdef3339f7a77
M <603803799@qq.com>
2019-05-09 14:59:52 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /*!
- * calculation.js v1.0.0
+ * calculation.js v1.0.1
  * 2017 603803799@qq.com
  * Released under the MIT License.
  */
@@ -10,61 +10,32 @@
 })(this, function () {
   return {
     division(arg1, arg2) { //除法
-      var t1 = 0, t2 = 0, r1, r2;
-      try {
-        t1 = arg1.toString().split(".")[1].length
-      } catch (e) {
-        t1 = 0
-      }
-      try {
-        t2 = arg2.toString().split(".")[1].length
-      } catch (e) {
-        t2 = 0
-      }
-      if (Math) return r1 = Number(arg1.toString().replace(".", "")), r2 = Number(arg2.toString().replace(".", "")), this.multiplication((r1 / r2), Math.pow(10, t2 - t1));
-      else return false
+      var t1 = String(arg1).split(".")[1] ? String(arg1).split(".")[1].length : 0,
+          t2 = String(arg2).split(".")[1] ? String(arg2).split(".")[1].length : 0, 
+          r1 = Number(String(arg1).replace(".", "")), 
+          r2 = Number(String(arg2).replace(".", ""));
+      
+      return this.multiplication((r1 / r2), Math.pow(10, t2 - t1));
     },
     multiplication(arg1, arg2) {//乘法
-      var m = 0, s1 = arg1.toString(), s2 = arg2.toString();
-      try {
-        m += s1.split(".")[1].length
-      } catch (e) {
-        m += 0
-      }
-      try {
-        m += s2.split(".")[1].length
-      } catch (e) {
-        m += 0
-      }
+      var s1 = String(arg1), s2 = String(arg2), m = s1.split(".")[1] ? s1.split(".")[1].length : 0;
+      m += s2.split(".")[1] ? s2.split(".")[1].length : 0;
       return Number(s1.replace(".", "")) * Number(s2.replace(".", "")) / Math.pow(10, m)
     },
     addition(arg1, arg2) {//加法
-      var r1, r2, m;
-      try {
-        r1 = arg1.toString().split(".")[1].length
-      } catch (e) {
-        r1 = 0;
-      }
-      try {
-        r2 = arg2.toString().split(".")[1].length
-      } catch (e) {
-        r2 = 0;
-      }
-      return m = Math.pow(10, Math.max(r1, r2)), (arg1 * m + arg2 * m) / m
+      var r1 = String(arg1).split(".")[1] ? String(arg1).split(".")[1].length : 0, 
+          r2 = String(arg2).split(".")[1] ? String(arg2).split(".")[1].length : 0,
+          m = Math.pow(10, Math.max(r1, r2));
+     
+      return (arg1 * m + arg2 * m) / m
     },
     subtraction(arg1, arg2) {//减法
-      var r1, r2, m, n;
-      try {
-        r1 = arg1.toString().split(".")[1].length
-      } catch (e) {
-        r1 = 0;
-      }
-      try {
-        r2 = arg2.toString().split(".")[1].length
-      } catch (e) {
-        r2 = 0;
-      }
-      return m = Math.pow(10, Math.max(r1, r2)), n = (r1 >= r2) ? r1 : r2, ((arg1 * m - arg2 * m) / m).toFixed(n);
+      var r1 = String(arg1).split(".")[1] ? String(arg1).split(".")[1].length : 0,
+          r2 = String(arg2).split(".")[1] ? String(arg2).split(".")[1].length : 0,
+          m =  Math.pow(10, Math.max(r1, r2)), 
+          n = (r1 >= r2) ? r1 : r2;
+      
+      return Number(((arg1 * m - arg2 * m) / m).toFixed(n));
     }
   }
 });
