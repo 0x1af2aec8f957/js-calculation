faa70c2a1bc15b94780932a8ba3586a7199b7547
M <0x23fd5097@gmail.com>
2019-10-30 11:12:21 +0800

    feat: Math相关方法

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-> calculation.js@2.0.0
+> calculation.js@2.0.1
 
 ##### matters needing attention
 
@@ -37,4 +37,4 @@ calc('0.1 0.1 * 0.1 - 0.1 0.1 * +') // -0.08
 
 [MIT](http://opensource.org/licenses/MIT)
 
-[calculation.js@2.0.0](https://github.com/noteScript/js-calculation.git)
+[calculation.js@2.0.1](https://github.com/noteScript/js-calculation.git)
--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 /*!
- * calculation.js v2.0.0
+ * calculation.js v2.0.1
  * 2017 603803799@qq.com
  * Released under the MIT License.
  */
 
 const IS_NUMBER = /^\-?\d*\.?\d?$/;
+const CONS_NUMBER = ['E', 'LN2', 'LN10', 'LOG2E', 'LOG10E', 'PI', 'SQRT1_2', 'SQRT2'];
 
-const calc = {
+export const calc = {
     '/': function (arg1, arg2) { // 除法
         const t1 = String(arg1).split('.')[1] ? String(arg1).split('.')[1].length : 0;
         const t2 = String(arg2).split('.')[1] ? String(arg2).split('.')[1].length : 0;
@@ -18,8 +19,7 @@ const calc = {
     '*': function (arg1, arg2) { // 乘法
         const s1 = String(arg1);
         const s2 = String(arg2);
-        let
-            m = s1.split('.')[1] ? s1.split('.')[1].length : 0;
+        let m = s1.split('.')[1] ? s1.split('.')[1].length : 0;
         m += s2.split('.')[1] ? s2.split('.')[1].length : 0;
         return Number(s1.replace('.', '')) * Number(s2.replace('.', '')) / (10 ** m);
     },
@@ -38,13 +38,53 @@ const calc = {
 
         return Number(((arg1 * m - arg2 * m) / m).toFixed(n));
     },
+    '%': function (arg1, arg2) { // 余数
+        const r1 = String(arg1).split('.')[1] ? String(arg1).split('.')[1].length : 0;
+        const r2 = String(arg2).split('.')[1] ? String(arg2).split('.')[1].length : 0;
+        const m = 10 ** Math.max(r1, r2);
+        return this['*'](arg1, m) % this['*'](arg2, m) / m;
+    },
+    '**': function (arg1, arg2) { // 幂运算
+        const r1 = String(arg1).split('.')[1] ? String(arg1).split('.')[1].length : 0;
+        return (arg1 ** arg2).toFixed(this['*'](r1, arg2))
+    },
+    'imul': function (arg1, arg2) {
+        return Math.imul(arg1, arg2)
+    },
+    'hypot': function (arg1, arg2) {
+        return Math.hypot(arg1, arg2)
+    },
+    'atan2': function (arg1, arg2) {
+        return Math.atan2(arg1, arg2)
+    },
+    'max': function (arg1, arg2) {
+        return Math.max(arg1, arg2)
+    },
+    'min': function (arg1, arg2) {
+        return Math.min(arg1, arg2)
+    },
 };
 
 export default str => {
     const OPERATORS = Object.keys(calc);
     return str.split(' ').reduce((acc, cur) => {
-        if (OPERATORS.includes(cur)) acc.push(calc[cur](acc.pop(), acc.pop()));
-        else if (IS_NUMBER.test(cur)) acc.push(cur);
+        switch (true) {
+            case OPERATORS.includes(cur):
+                acc.push(calc[cur](acc.pop(), acc.pop()));
+                break;
+            case IS_NUMBER.test(cur):
+                acc.push(cur);
+                break;
+            case CONS_NUMBER.includes(cur):
+                acc.push(Math[cur]);
+                break;
+            case Object.hasOwnProperty.call(Math, cur):
+                acc.push(Math[cur](acc.pop()));
+                break;
+            default:
+                break;
+        }
+
         return acc;
     }, []).shift();
 };
